0178fa5ae4bdc478898b51a76c9e6463e8d68967
DESKTOP-NUOPUIF\ITPS <itps@google.com>
2022-05-26 21:55:53 +0900

    0526백엔드 배열 배움

--- javaStudy_juhyeon/src/j10_클래스기본정리/Controller/UserManagementController.java
+++ javaStudy_juhyeon/src/j10_클래스기본정리/Controller/UserManagementController.java
@@ -5,18 +5,36 @@ import j10_클래스기본정리.Service.UserService;
 
 public class UserManagementController {
 	private UserService userService;
+	private User[] users;
 	
 	public UserManagementController(UserService userService) {
 		userService = new UserService();
+		users = new User[2];
 	}
 
 	public void createUser() {
-		User user = userService.insertUser();               //유저객체만 입력가능
+		int indexResult = findEmptyInUsers();//유저객체만 입력가능
+		if(indexResult != -1) {
+			users[0] = userService.insertUser();
+			
+			
+			System.out.println("[새로 추가된 사용자]");
+			System.out.println(users[indexResult].toString());
+			
+		} else {
+			System.out.println("더이상 사용자를 추가할 수 없습니다.");
+		}
 		
-		System.out.println("[새로 추가된 사용자]");
-		System.out.println(user.toString());
-	}
 	
+	}
+	private int findEmptyInUsers() {
+		for(int i = 0; i<users.length; i++) {
+			if(users[i]==null) {
+				return i;
+			}
+		}
+		return -1;
+	}
 	public void getUser() {
 		
 		
---
+++ javaStudy_juhyeon/src/j11_자바배열/Array02.java
@@ -0,0 +1,45 @@
+package j11_자바배열;
+
+public class Array02 {
+
+	public static void main(String[] args) {
+//		int size = 4;
+//		int[] numbers = new int[size];
+//		
+//		System.out.println(numbers.length);
+		/*
+		 *  for문 으로 0-99까지 반복을 한다.
+		 *  변수 i의 값이 8의 배수이면 
+		 *  8의 배수 갯수만큼 배열을 생성하고, 
+		 *  8의 배수를 모두 배열에 대입한다.
+		 *  배열에 들어있는 8의 배수를 모두 출력하시오.
+		 * 
+		 * 
+		 */
+		
+		int size = 0;
+		
+		for(int i = 0; i<100;i++) {  // 처음에 0으로 나누지 말아라 는 조건
+			if(i != 0 && i % 8 == 0) {
+				size++;
+			}
+		}
+		
+		
+		int[] numbers = new int[size];
+		
+		for(int i = 0, j = 0; i<100 ; i++) {
+			if(i !=0 && i % 8 == 0) {
+				numbers[j] = i;
+				j++; //j++ 후증가 라서 i가 8일때 j는 0임 => i가 9일때 j는 1이 되는것.
+			}
+	
+			
+		}
+		for(int i = 0; i<numbers.length; i++) {
+			System.out.println(numbers[i]);
+		}
+		
+	}
+
+}
